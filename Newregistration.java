af11abf50d026e6d4e76d1100f1a7f31c9a3cf5b
DESKTOP-UP1GJE3\uOhmac Technologies <puttursunil@gmail.com>
2017-06-12 16:14:29 +0530

    Initial commit

---
+++ Newregistration.java
@@ -0,0 +1,22 @@
+package app.e_commerce.com.msupply;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+/**
+ * Created by uOhmac Technologies on 07-Jun-17.
+ */
+public class Newregistration extends AppCompatActivity {
+
+
+    EditText etfirstname,etlastname,etemail,etpassword,etconfirmpwd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+        etfirstname = (EditText)findViewById(R.id.et_firstname);
+        etlastname = (EditText)findViewById(R.id.et_lastname);
+    }
+}
